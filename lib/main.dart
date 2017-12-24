25fa76db30465528c7a0122896c258c3d00eae03
jpgpuyo <jpgpuyo@gmail.com>
2017-12-24 02:29:08 +0100

    6. Add a UI for displaying messages - Place the message list

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,26 @@ class ChatScreenState extends State<ChatScreen> {
       appBar: new AppBar(
           title: new Text("Friendlychat")
       ),
-      body: _buildTextComposer(),
+      body: new Column(
+        children: <Widget>[
+          new Flexible(
+            child: new ListView.builder(
+              padding: new EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, int index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          new Divider(height: 1.0),
+          new Container(
+            decoration: new BoxDecoration(
+                color: Theme
+                    .of(context)
+                    .cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
     );
   }
 
@@ -66,6 +85,12 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 }
 
